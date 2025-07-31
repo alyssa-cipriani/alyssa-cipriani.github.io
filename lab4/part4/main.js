f51af500e4ba7101af16d4bb71c6fd6de8ec9372
alyssa-cipriani <alyssa.cipriani@dcmail.ca>
2025-07-31 12:48:19 -0400

    making const insertX

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -7,14 +7,9 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-2. RAW TEXT STRINGS
 
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
-
-Willy the Goblin
-Big Daddy
-Father Christmas
-
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+const insertX = ['Willy the Goblin','Big Daddy','Father Christmas']
 the soup kitchen
 Disneyland
 the White House
