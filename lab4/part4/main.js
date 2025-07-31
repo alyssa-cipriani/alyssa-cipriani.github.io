b7b074cdfaf87a79fad0e2628d3b302a331fb137
alyssa-cipriani <alyssa.cipriani@dcmail.ca>
2025-07-31 12:49:51 -0400

    made const insertY

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -10,9 +10,8 @@ function randomValueFromArray(array){
 
 const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
 const insertX = ['Willy the Goblin','Big Daddy','Father Christmas']
-the soup kitchen
-Disneyland
-the White House
+const insertY =['the soup kitchen','Disneyland','the White House']
+
 
 spontaneously combusted
 melted into a puddle on the sidewalk
