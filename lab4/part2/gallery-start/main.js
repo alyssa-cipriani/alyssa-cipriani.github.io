3799e0ea382f9613ff1fb66a577190905004b0a0
alyssa-cipriani <alyssa.cipriani@dcmail.ca>
2025-07-31 17:27:02 -0400

    fixed mistakes

--- lab4/part2/gallery-start/main.js
+++ lab4/part2/gallery-start/main.js
@@ -5,7 +5,7 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images - ["pic1.jpg", "pic2.jpg", "p1c3.jpg", "pic4.jpg", "pic5.jpg"];
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
 const alts ={
@@ -14,11 +14,12 @@ const alts ={
 
 "pic2.jpg": "textured rock",
 
-"pic3.jpg": "white and purple flowers"
+"pic3.jpg": "white and purple flowers",
 
 "pic4.jpg": "egyptian hieroglyphs",
 
-"pic5.jpg": "brown moth on a leaf"};
+"pic5.jpg": "brown moth on a leaf"
+}
 
 /* Looping through images */
 for (let i=0;i<images.length;i++){
@@ -35,8 +36,10 @@ for (let i=0;i<images.length;i++){
 btn.addEventListener('click',function(){
 	if(btn.textContent == "Darken"){
 		btn.textContent = "Lighten"
-		overlay.style.backgroundColor = 'rgba (0,0,0,0.5)';
+		overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
 	}
 	else{
-		btn.txtContent = "Darken"
+		btn.txtContent = "Darken";
 		overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+	}
+});
