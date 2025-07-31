227ed611b0f218217bc06eff66835bddea796dc6
alyssa-cipriani <alyssa.cipriani@dcmail.ca>
2025-07-31 13:07:31 -0400

    making placeholders

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -9,7 +9,7 @@ function randomValueFromArray(array){
 
 
 const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
-const insertX = ['Willy the Goblin','Big Daddy','Father Christmas']
+const insertX =['Willy the Goblin','Big Daddy','Father Christmas']
 const insertY =['the soup kitchen','Disneyland','the White House']
 const insertZ =['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away']
 
@@ -23,9 +23,14 @@ function result() {
 	const xItem = randomValueFromArray(insertX);
 	const yItem = randomValueFromArray(insertY);
 	const zItem = randomValueFromArray(insertZ);
+	
+	newStory = newStory.replaceAll(':insertx:', xItem);
+	newStory = newStory.replaceAll(':inserty:', yItem);
+	newStory = newStory.replaceAll(':insertz:', zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
+	newStory = newStory.replaceAll('Bob',name);
 
   }
 
