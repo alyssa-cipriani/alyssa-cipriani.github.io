8c68ba21f44083f04180b08f9e66eb4980fecf46
alyssa-cipriani <alyssa.cipriani@dcmail.ca>
2025-08-01 22:45:00 -0400

    Update main.js

--- lab4/part2/gallery-start/main.js
+++ lab4/part2/gallery-start/main.js
@@ -9,17 +9,13 @@ const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
 const alts ={
+  "pic1.jpg": "close up of human eye",
+  "pic2.jpg": "textured rock",
+  "pic3.jpg": "white and purple flowers",
+  "pic4.jpg": "Egyptian hieroglyphs",
+  "pic5.jpg": "brown moth on a leaf"
+};
 
-"pic1.jpg": "close up of human eye ",
-
-"pic2.jpg": "textured rock",
-
-"pic3.jpg": "white and purple flowers",
-
-"pic4.jpg": "Egyptian hieroglyphs",
-
-"pic5.jpg": "brown moth on a leaf"
-}
 
 /* Looping through images */
 for (let i = 0; i < images.length; i++) {
@@ -35,13 +31,12 @@ for (let i = 0; i < images.length; i++) {
 }  
 
 /* Wiring up the Darken/Lighten button */
-btn.addEventListener('click',function(){
-	if(btn.textContent === "Darken"){
-		btn.textContent = "Lighten"
-		overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-	}
-	else{
-		btn.txtContent = "Darken";
-		overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-	}
+btn.addEventListener('click', function() {
+  if (btn.textContent === "Darken") {
+    btn.textContent                = "Lighten";
+    overlay.style.backgroundColor  = 'rgba(0,0,0,0.5)';
+  } else {
+    btn.textContent                = "Darken";
+    overlay.style.backgroundColor  = 'rgba(0,0,0,0)';
+  }
 });
