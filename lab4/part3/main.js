ed4bc9bcb5e740697dd003c36165829cce7d11e7
alyssa-cipriani <alyssa.cipriani@dcmail.ca>
2025-08-02 20:08:12 -0400

    drawing the ball

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -27,4 +27,12 @@ class Ball {
     this.size = size;
   }
 }
-		
+class Ball {
+  // …
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}		
