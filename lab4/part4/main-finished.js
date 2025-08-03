a34531213a5c14338398a0c2c5bfc8c5b9e5a2d8
alyssa-cipriani <alyssa.cipriani@dcmail.ca>
2025-08-02 21:43:24 -0400

    checking bounds

--- lab4/part4/main-finished.js
+++ lab4/part4/main-finished.js
@@ -93,6 +93,20 @@ draw(){
     ctx.stroke();
 }
 
+checkBounds(){
+    if (this.x + this.size >= width){
+      this.x -= this.size;
+    }
+    if (this.x - this.size <= 0){
+      this.x += this.size;
+    }
+    if (this.y + this.size >= height){
+      this.y -= this.size;
+    }
+    if (this.y - this.size <= 0){
+      this.y += this.size;
+    }
+  }
  setControls() {
 	 window.addEventListener("keydown", (e) => {
 		 switch (e.key) {
