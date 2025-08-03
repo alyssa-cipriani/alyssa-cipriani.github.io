b8801af007322ffbef2028237c4c3c3e1d9e5e28
alyssa-cipriani <alyssa.cipriani@dcmail.ca>
2025-08-02 21:30:54 -0400

    adding ball count

--- lab4/part4/main-finished.js
+++ lab4/part4/main-finished.js
@@ -6,7 +6,8 @@ const ctx = canvas.getContext("2d");
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
 
-
+const para = document.querySelector('p');
+let count = 0;
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
